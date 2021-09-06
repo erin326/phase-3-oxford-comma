fab75c8c9c7b1798e747a35ed24a27ac36cb4418
erin <erinverkoulen93@gmail.com>
2021-09-06 18:06:23 -0500

    commit tree 651ac3db3aadd74356eef3fcf94c04cce2bf2107

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length == 2
+        array.join(' and ')
+    elsif array.length >= 3
+        last_element = array.pop
+        string = array.join(', ')
+        "#{string}, and #{last_element}"
+    else array.join
+    end
 end
